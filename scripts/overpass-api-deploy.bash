117ac7b3e1461011617b40071a7f578bd18b9e0f
nick <nick@cyclops.home>
2021-08-09 19:36:18 +0100

    adding script to support overpass api docker

---
+++ scripts/overpass-api-deploy.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+function validate_url(){
+  if [[ `wget -S --spider $1  2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then
+    return 0
+  else
+    echo "$2 does not exit"
+    exit 1
+  fi
+}
+
+ dockerName=$1
+ region=$2
+ port=$3 
+
+
+ overpassUrl="http://download.geofabrik.de/europe/${region}-latest.osm.bz2"
+ validate_url "${overpassUrl}" "${region}" 
+ 
+# overpassDiff="http://download.openstreetmap.fr/replication/europe/${region}/minute/"
+# validate_url "${region}" "${overpassDiff}"
+
+ hostStore="/data/docker/overpass_db/${region}/"
+
+ echo "deploying $dockerName $region $port $hostStore"
+
+ docker run \
+  -e OVERPASS_META=yes \
+  -e OVERPASS_MODE=init \
+  -e OVERPASS_PLANET_URL="${overpassUrl}" \
+  -e OVERPASS_RULES_LOAD=10 \
+  -v "${hostStore}":/db \
+  -p "${port}":80 \
+  -i -t \
+  --name "${dockerName}" wiktorn/overpass-api
+
---
+++ scripts/overpass-api.bash
@@ -0,0 +1,80 @@
+#!/bin/bash
+
+help() {
+  echo "script to controll docker internaces of overpass api"
+  echo "usage : overpass-api.bash [cmd] [area]"
+  echo "cmd : [deploy|start|stop]  "
+  echo "area : [ireland | great-britain | isle-of-man | guernsey-jersey | wales | scotland | england] "
+
+}
+
+
+
+cmd=$1
+region=""
+rootPort=7000
+echo $1 $2
+
+dockerName="overpass-${2}"
+
+case "$2" in 
+
+  great-britain)
+   region="great-britain"
+   apiPort= rootPort
+   ;;
+  ireland-and-northen-ireland)
+   region="irenland-and-northen-ireland"
+   apiPort=$(( $rootPort+1))
+   ;;
+  isle-of-man)
+    region="isle-of-man"
+    apiPort=$(($rootPort+2))
+    ;;
+ guernsey-jersey)
+    region="guernsey-jersey"
+    apiPort=$(($rootPort+3))
+    ;;
+  england)
+    region="great-britain/england"
+    apiPort=$(($rootPort+4))
+    ;;
+  wales) 
+    region="great-britain/wales"
+    apiPort=$(($rootPort+5))
+    ;;
+   scotland)
+     region="great-britain/scotland"
+     apiPort=$(($rootPort+6))
+     ;; 
+   hampshire)
+     region="great-britain/england/hampshire"
+     apiPort=$(($rootPort+7))
+
+     ;;
+ *)
+   echo "region $2 not supported"
+   help
+   exit 1
+esac
+
+echo $region $apiPort
+
+case "$1" in 
+  deploy) 
+    ./overpass-api-deploy.bash ${dockerName} ${region} ${port}
+    ;; 
+  start)
+    echo "restarting ${dockerName}"
+    docker restart ${dockerName}
+    ;;
+  stop)
+    echo "stop ${dockerName}"
+    docker stop ${dockerName}
+    ;;
+  *)
+    echo "cmd $1 not supported"
+    help
+    exit 1
+esac
+
