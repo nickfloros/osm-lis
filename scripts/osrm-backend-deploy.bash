b1ca54c037f53c1ed84a5879c7c1588c9df3c9b3
nick <nick@cyclops.home>
2021-08-09 23:28:34 +0100

    adding backend deploy

--- scripts/osrm-backend-deploy.bash
+++ scripts/osrm-backend-deploy.bash
@@ -1,22 +1,37 @@
 #!/bin/bash
 
 area=$1
+dataStore=$2/data/${area}
 
-if [ ! -d "${PWD}/data/${area}" ]
+function validate_url(){
+  if [[ `wget -S --spider $1  2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then
+    return 0
+  else
+    echo "$2 does not exit"
+    exit 1
+  fi
+}
+
+
+if [ ! -d "${dataStore}" ]
 then
-	echo ${PWD}/${area} does not exist
+    echo "creating ${dataStore}"
+    mkdir -p ${dataStpre}
+fi
+
+if [ ! -f "${dataStore}/${area}-latest.osm.pbf" ]
+then
+    echo "${dataStore}/${area}-latest.osm.pbf does not exist
     echo aborting
     exit 1
 fi
 
-if [ ! -f "${PWD}/data/${area}/${area}-latest.osm.pbf" ]
-then
-	echo ${PWD}/${area}/${area}-latest.osm.pbf does not exist
-    echo aborting
-    exit 1
-fi
+pbfUrl="http://download.geofabrik.de/europe/${area}-latest.osm.pbf"
+validate_url "${osrmUrl}" "${area}" 
 
-docker run -t -v "${PWD}/data/${area}:/data" osrm/osrm-backend osrm-extract -p /opt/car.lua /data/${area}-latest.osm.pbf
+wget -D ${dataStore}/${area}-latest.osm.pbf  ${pbfUrl}
+ 
+docker run -t -v "${dataStore}:/data" osrm/osrm-backend osrm-extract -p /opt/car.lua /data/${area}-latest.osm.pbf
 
-docker run -t -v "${PWD}/data/${area}:/data" osrm/osrm-backend osrm-partition /data/${area}-latest.osrm
-docker run -t -v "${PWD}/data/${area}:/data" osrm/osrm-backend osrm-customize /data/${area}-latest.osrm
+docker run -t -v "${dataStore}:/data" osrm/osrm-backend osrm-partition /data/${area}-latest.osrm
+docker run -t -v "${dataStore}:/data" osrm/osrm-backend osrm-customize /data/${area}-latest.osrm
