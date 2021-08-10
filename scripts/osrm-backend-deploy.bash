de6c4ce6165bd0cb024862b1993763fc072daf76
nick <nick@cyclops.home>
2021-08-10 22:26:19 +0100

    working deploy script

--- scripts/osrm-backend-deploy.bash
+++ scripts/osrm-backend-deploy.bash
@@ -1,37 +1,58 @@
 #!/bin/bash
 
-area=$1
-dataStore=$2/data/${area}
+region=$1
+rootStore=$2
+port=$3
+dataStore=${rootStore}/${region}
 
 function validate_url(){
   if [[ `wget -S --spider $1  2>&1 | grep 'HTTP/1.1 200 OK'` ]]; then
     return 0
   else
-    echo "$2 does not exit"
+    echo "region $2 does not exit in $1"
     exit 1
   fi
 }
 
+function help() {
+  echo "osrm-backend-deploy.bash [region] [rootStore] [port]"
+  echo "region : opensteet map region for which we want to host osrm backend"
+  echo "rootStore : is the root location where we want to permanetly store the osrm dataset for the region identified above. The data will be stored in \$rootStore/\$region" 
+  echo "port : is the host port we will docker will accept requests"
+}
 
-if [ ! -d "${dataStore}" ]
-then
-    echo "creating ${dataStore}"
-    mkdir -p ${dataStpre}
+if [ ! -d "${rootStore}" ]; then 
+	echo "rootStore in ${rootStore} not found ... aborting"
+	help
+	exit 1
 fi
 
-if [ ! -f "${dataStore}/${area}-latest.osm.pbf" ]
-then
-    echo "${dataStore}/${area}-latest.osm.pbf does not exist
+if [ ! -d "${dataStore}" ]; then
+    echo "creating ${dataStore}"
+    mkdir -p ${dataStore}
+fi
+
+latest=${region}-latest
+pbf=${latest}.osm.pbf
+osrm=${latest}.osrm
+
+pbfUrl=http://download.geofabrik.de/europe/${pbf}
+
+validate_url ${pbfUrl} ${region}
+
+wget -O ${dataStore}/${region}-latest.osm.pbf ${pbfUrl}
+
+if [ ! -f "${dataStore}/${pbf}" ]; then
+    echo "${dataStore}/${pbf} does not exist"
     echo aborting
     exit 1
 fi
 
-pbfUrl="http://download.geofabrik.de/europe/${area}-latest.osm.pbf"
-validate_url "${osrmUrl}" "${area}"
+ 
+docker run -t -v ${dataStore}:/data osrm/osrm-backend osrm-extract -p /opt/car.lua /data/${pbf}
 
-wget -D ${dataStore}/${area}-latest.osm.pbf  ${pbfUrl}
+docker run -t -v ${dataStore}:/data osrm/osrm-backend osrm-partition /data/${osrm}
+docker run -t -v ${dataStore}:/data osrm/osrm-backend osrm-customize /data/${osrm}
 
-docker run -t -v "${dataStore}:/data" osrm/osrm-backend osrm-extract -p /opt/car.lua /data/${area}-latest.osm.pbf
+docker run -d -i -p ${port}:5000 -v "${dataStore}:/data" --name osrm-backend-${region} osrm/osrm-backend osrm-routed --algorithm mld /data/${osrm}
 
-docker run -t -v "${dataStore}:/data" osrm/osrm-backend osrm-partition /data/${area}-latest.osrm
-docker run -t -v "${dataStore}:/data" osrm/osrm-backend osrm-customize /data/${area}-latest.osrm
